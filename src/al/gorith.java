95b7c87eea30fd86193ca2dcde2b126a334584cc
gaelxxl34 <107554725+gaelxxl34@users.noreply.github.com>
2022-09-15 21:52:54 +0100

    the original version

---
+++ src/al/gorith.java
@@ -0,0 +1,28 @@
+package al;
+
+import java.util.Scanner;
+
+public class gorith
+{
+    public static void main(String[] args)
+    {
+        Scanner calcul = new Scanner(System.in);
+        int b, n;
+        System.out.println("enter the base");
+        b= calcul.nextInt();
+        System.out.println("enter power");
+        n = calcul.nextInt();
+        int pr = 1;
+
+        for (int i = b; i  <= b; i++)
+        {
+            for(int x = 1; x <= n; x++)
+            {
+
+                pr = pr * b;
+
+            }
+        }
+        System.out.println(pr);
+    }
+}
