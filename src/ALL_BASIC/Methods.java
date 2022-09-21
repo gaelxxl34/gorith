f5101b6cf0883a4ce88cc2be18968ab5c7272394
gaelxxl34 <107554725+gaelxxl34@users.noreply.github.com>
2022-09-21 08:56:44 +0100

    first program addition and division
    Co-Authored-By: Gael Lazzari <1110765+gael@users.noreply.github.com>

--- src/ALL_BASIC/Methods.java
+++ src/ALL_BASIC/Methods.java
@@ -4,6 +4,9 @@ public class Methods
 {
     public static void main(String[] args)
     {
-        
+        int a = 10;
+        int b = 30;
+        int c = a + b / 2;
+        System.out.println("the answer is: " + c);
     }
 }
